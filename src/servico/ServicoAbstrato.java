a8fe5bdaaafde4e4356854c2e7a4656d2d551d29
not <henriquefrinhani@hotmail.com>
2017-07-01 16:58:12 -0300

    teste

--- src/servico/ServicoAbstrato.java
+++ src/servico/ServicoAbstrato.java
@@ -14,6 +14,7 @@ import mensagem.TipoMensagem;
 import util.ObjetoUtil;
 
 public abstract class ServicoAbstrato {
+//teste
 
     protected static Connection obterConnection() throws SystemException {// Analizar 
         try {
--- src/testes/TesteIdade.java
+++ src/testes/TesteIdade.java
@@ -6,5 +6,6 @@ public class TesteIdade {
     public static void main(String[] args){
   
         System.out.println(IdadeUtil.getIdade());
+//teste
     }
 }
